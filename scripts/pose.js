924a8db39180f790e04a56acb1eb725e4b1ff195
unknown <knosmos@gmail.com>
2021-10-23 15:47:56 -0400

    Add leg lateral motion detection

--- scripts/pose.js
+++ scripts/pose.js
@@ -4,8 +4,9 @@ let canvasCtx = canvasElement.getContext('2d');
 let gestureDisplay = document.getElementById('gesture');
 
 let x_threshold = 0.15;
-let y_threshold = 0.15;
+let y_threshold = 0.45;
 let squat_threshold = 1.25;
+let kick_threshold = 0.15;
 
 let gesture = "none";
 let loaded = false;
@@ -13,6 +14,7 @@ let loaded = false;
 let showCamera = true; // whether to overlay camera view on pose detection canvas
 
 let squatForDown = false; // whether down motion is triggered by squatting
+let kickForLateral = false; // whether left/right motion is triggered by kicking
 
 function onResults(results) {
     canvasCtx.save();
@@ -47,28 +49,54 @@ function onResults(results) {
         let rightKnee = results.poseLandmarks[26];
         let leftKnee = results.poseLandmarks[25];
 
+        let rightHip = results.poseLandmarks[24];
+        let leftHip = results.poseLandmarks[23];
+
         let x_diff = (leftHand.x - leftShoulder.x) + (rightHand.x - rightShoulder.x);
         let y_diff = (leftHand.y - leftShoulder.y) + (rightHand.y - rightShoulder.y);
-        let s_diff = (leftKnee.y - leftShoulder.y) + (rightKnee.y - rightShoulder.y);
-        //console.log(s_diff);
+        
+        // let s_diff = (leftKnee.y - leftShoulder.y) + (rightKnee.y - rightShoulder.y);
+
+        let r_leg_diff = Math.abs(rightKnee.y - rightHip.y);
+        let l_leg_diff = Math.abs(leftKnee.y - leftHip.y);
 
         gesture = "none";
-        if (x_diff > x_threshold){
-            gesture = "left";
-        }
-        else if (-x_diff > x_threshold){
-            gesture = "right";
-        }
-        else if (-y_diff > y_threshold){
+
+        if (-y_diff > y_threshold){
             gesture = "up";
         }
-        else if (squatForDown && s_diff < squat_threshold){
-            gesture = "down";
+
+        if (!squatForDown) {
+            if (y_diff > y_threshold){
+                gesture = "down";
+            }            
         }
-        else if (!squatForDown && y_diff > y_threshold){
-            gesture = "down";
-        }        
+
+        if (kickForLateral) {
+            if (r_leg_diff < kick_threshold && l_leg_diff > kick_threshold){
+                gesture = "right";
+            }
+            else if (l_leg_diff < kick_threshold){
+                gesture = "left";
+            }
+        }
+        else {
+            if (x_diff > x_threshold){
+                gesture = "left";
+            }
+            else if (-x_diff > x_threshold){
+                gesture = "right";
+            }
+        }
         
+        if (squatForDown) {
+            let s_diff = r_leg_diff + l_leg_diff;
+            console.log (Math.abs(r_leg_diff-l_leg_diff));
+            if (s_diff < squat_threshold && Math.abs(r_leg_diff-l_leg_diff) < 0.05){
+                gesture = "down";
+            }
+        }
+
         gestureDisplay.innerHTML = gesture;        
     }
 }
